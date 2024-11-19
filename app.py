e75314dbc0f2cc633d41d5737ec0d21a525f1e8e
Prudence njenga <prudencenjenga99@gmail.com>
2024-11-19 12:07:04 +0300

    Modified requirements.txt

--- app.py
+++ app.py
@@ -23,4 +23,7 @@ def generate_report():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use an environment variable to determine if debug mode is enabled
+    # Use the PORT environment variable provided by Render, fallback to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
